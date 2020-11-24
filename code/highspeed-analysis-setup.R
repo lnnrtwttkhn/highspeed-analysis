05e4773b5239b9da29937f42f32b13a450560a22
Lennart Wittkuhn <wittkuhn@mpib-berlin.mpg.de>
2020-11-24 10:14:31 +0100

    add code for statistical analyses of oddball data

--- code/highspeed-analysis-setup.R
+++ code/highspeed-analysis-setup.R
@@ -33,11 +33,11 @@ path_participants <- file.path(path_root, "data", "bids", "participants.tsv")
 # path to the events.tsv files (according to BIDS):
 path_events <- file.path(path_root, "data", "bids", "*", "*", "func", "*events.tsv")
 # path to data from the decoding analysis:
-path_pred <- file.path(path_root, "data", "decoding", "*", "data", "*decoding.csv")
+path_pred <- file.path(path_root, "data", "decoding", "decoding", "*", "data", "*decoding.csv")
 # path to the data from the mask thresholding:
-path_thresh <- file.path(path_root, "data", "decoding", "*", "data", "*thresholding.csv")
+path_thresh <- file.path(path_root, "data", "decoding", "decoding", "*", "data", "*thresholding.csv")
 # path to the data of the voxel patterns:
-path_patterns <- file.path(path_root, "data", "decoding", "*", "data", "*voxel_patterns_union.csv")
+path_patterns <- file.path(path_root, "data", "decoding", "decoding", "*", "data", "*voxel_patterns_union*.csv")
 # Load all [BIDS](http://bids.neuroimaging.io/) events files:
 # read all events files and concatenate them in a data table:
 # datalad get data/bids/sub-*/ses-*/func/*events.tsv
@@ -48,13 +48,18 @@ dt_events[, by = .(subject, condition, trial), ":=" (
   trial_accuracy = accuracy[!is.na(accuracy)]
 )]
 # read all decoding data files and concatenate them in a data table:
-#dt_pred <- do.call(rbind, lapply(Sys.glob(path_pred), data.table::fread))
+# datalad get data/decoding/decoding/sub-*/data/*_decoding.csv
+dt_pred <- do.call(rbind, lapply(Sys.glob(path_pred), data.table::fread))
 # prepare data (add additional variables like a speed trial counter etc.)
-#dt_pred <- prepare_data(dt_pred)
+dt_pred <- prepare_data(dt_pred)
 # read all data from the mask thresholding:
-#dt_thresh = do.call(rbind, lapply(Sys.glob(path_thresh), data.table::fread))
+# datalad get data/decoding/decoding/*/data/*thresholding.csv
+dt_thresh = do.call(rbind, lapply(Sys.glob(path_thresh), data.table::fread))
 # read all data from the mask thresholding:
-#dt_patterns = lapply(Sys.glob(path_patterns), data.table::fread)
+# datalad get data/decoding/decoding/*/data/*voxel_patterns_union*.csv
+files_patterns = Sys.glob(path_patterns)
+files_patterns = files_patterns[!stringr::str_detect(files_patterns, "hpc")]
+#dt_patterns = lapply(files_patterns, data.table::fread)
 # create color list for probabilities of individual sequence events:
 color_events <- rev(hcl.colors(5, "Zissou 1"))
 # define global lmer settings used in all mixed effects lmer models:
